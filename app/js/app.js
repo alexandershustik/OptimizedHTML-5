b691670984650b14ba67195e8711c5d13f70d79d
Alexander Shustik <alexandershustik@gmail.com>
2023-09-26 19:23:46 +0200

    turkmen

--- app/js/app.js
+++ app/js/app.js
@@ -1,8 +1,22 @@
+/** @format */
+
 // // Import vendor jQuery plugin example
 // import '~/app/libs/mmenu/dist/mmenu.js'
+import intlTelInput from 'intl-tel-input';
 
 document.addEventListener('DOMContentLoaded', () => {
+    // Custom JS
+    const input = document.querySelector('.form__phone');
+    intlTelInput(input, {
+        initialCountry: 'tm',
+        onlyCountries: ['uz', 'tr', 'tm', 'ua', 'ru', 'by', 'kz'],
+    });
 
-	// Custom JS
+    const input1 = document.querySelector('.form__phone1');
+    intlTelInput(input1, {
+        initialCountry: 'tm',
+        onlyCountries: ['uz', 'tr', 'tm', 'ua', 'ru', 'by', 'kz'],
+    });
+});
 
-})
+//utilsScript: "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.13/js/utils.js",
